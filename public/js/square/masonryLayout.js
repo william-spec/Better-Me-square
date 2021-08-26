ae167d5e920649e8e5fc0c70e60871b876ffe08b
william-spec <52228900+william-spec@users.noreply.github.com>
2021-08-26 23:26:15 +0800

    Update masonryLayout.js

--- public/js/square/masonryLayout.js
+++ public/js/square/masonryLayout.js
@@ -3,6 +3,7 @@
 function getColumns(itemWidth = '200px'){    //获取当前窗口能放下的最大列数并返回
   let windowWidth = window.innerWidth * 0.9;    //可能会产生一些误差
   itemWidth = parseInt(itemWidth);
+  itemWidth += horiGap/2;   //一列的宽度由div的宽度还有间隙组成
   let columns = parseInt(windowWidth/itemWidth);
   return columns;
 }
@@ -38,15 +39,15 @@ function putItemsBiBao(){    //使用闭包避免将heights设置成全局变量
       else{
         if(heights.length < columns){    //第一行图片
           div.style.top = 0;   //顶格排放
-          div.style.left = index * itemWidth + index * 40 + 40 + 'px';
+          div.style.left = index * itemWidth + index * horiGap + horiGap + 'px';
           heights.push(parseInt(getComputedStyle(div).height));
         }
         else{   //其他行图片
           let minHeight = Math.min(...heights);   //获取所有列中的最小高度，优先摆放在高度最小的位置
           let minIndex = heights.indexOf(minHeight);    //获取最小高度的索引
-          div.style.top = minHeight + 50 + 'px';
-          div.style.left = minIndex * itemWidth + minIndex * 40 + 40 + 'px';
-          heights[minIndex] = minHeight + 50 + parseInt(getComputedStyle(div).height);   //更新高度
+          div.style.top = minHeight + vertGap + 'px';
+          div.style.left = minIndex * itemWidth + minIndex * horiGap + horiGap + 'px';
+          heights[minIndex] = minHeight + vertGap + parseInt(getComputedStyle(div).height);   //更新高度
         }
       }
     })
@@ -85,3 +86,5 @@ function delayLoad(){   //懒加载，只对图片懒加载，其他无src属性
 
 let putItems = putItemsBiBao();   //闭包函数调用
 let allDivs = [];   //全局数组，用于懒加载判断，保存所有已经加载的内容
+let horiGap = 40;   //水平间隙
+let vertGap = 50;   //垂直间隙
