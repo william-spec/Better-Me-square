0e79ffcb488a1c3d060419061fd7dec6e5a523f7
William <1833063260@qq.com>
2022-02-24 16:52:58 +0800

    添加忽视规则

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let server = http.createServer(router);
 // server.on('request', function(req, res){
 // 	router(req, res);
 // });
+
 //4.启动服务
 server.listen(3000, function(){
 	console.log('启动成功，访问：http://localhost:3000')
